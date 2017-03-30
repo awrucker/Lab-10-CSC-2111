fbe9badb439cbb540c90ced6620a9845ce5aa58a
Adam William Rucker <awrucker42@students.tntech.edu>
2017-03-30 10:00:44 -0500

    Fixed hybrid.h

--- Hybrid.h
+++ Hybrid.h
@@ -60,12 +60,8 @@ T* Hybrid<T>::dequeue()
 }
 ListDoublyLinkedIterator<T>* Hybrid::iterator()
 {
-<<<<<<< HEAD
 	ListDoublyLinkedIterator<T>* iterator =  sldl->iterator();
 	return iterator;
-=======
-
->>>>>>> 69bfd344f521398643f85c854c9cdbe616851184
 }
 
 
